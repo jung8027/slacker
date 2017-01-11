0f8bf09a8ffff1517f701fdb69eecd2b21812961
Samantha Bretous <sam@samanthabretous.com>
2017-01-11 17:51:10 -0500

    seed data

--- front/features/teamList/TeamList.jsx
+++ front/features/teamList/TeamList.jsx
@@ -6,6 +6,7 @@ const TeamList = props => {
   const {teams, user} = props;
 
   const showTeam = teamId => {
+    console.log(teamId, user.id)
     $.ajax({
       url: `/api/team/${teamId}/${user.id}`,
       type: 'GET'
@@ -18,7 +19,7 @@ const TeamList = props => {
   return (
     <section className="team_list">
       {teams && teams.map((team,index) =>{
-        return <p onClick={()=>showTeam(team.id)} className="team_img square" key={index}>{team.name[0] + team.name[1]}</p>
+        return <div onClick={()=>showTeam(team.id)} className="team_img square" key={index}><p>{team.name[0] + team.name[1]}</p></div>
       }
       )}
     </section>
