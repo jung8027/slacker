34c1d9e2447057d9b0639a9da9d629826c1a745e
jondubya86 <jonathanwu86@gmail.com>
2017-01-11 15:31:01 -0500

    newer changes

--- front/features/channel/Channel.jsx
+++ front/features/channel/Channel.jsx
@@ -3,28 +3,20 @@ import {Link} from 'react-router';
 import store from '../../store/store';
 // import {CHANNEL_UPDATE} from './channelActions';
 
-const enterChat = (param)=>{
-  console.log(param)
-};
-
 const Channel = (props)=> {
-  console.log(props)
   // enterChat()=>{
-  // console.log(this)
+  console.log(Object.keys(props))
   // }
     // Object.keys(props).map((k)=>console.log('from channel', k, props[k]))
     return (
       <div>
         <ul>
-        {props.userChannels.channel?
-          <li id={props.userChannels.channel.id}>{props.userChannels.channel.name}</li>
-          // onClick={this.enterChat.bind(this, a.username)}
-          :'Loading...'}
+
         </ul>
       </div>
     );
 }
-  
+          // onClick={this.enterChat.bind(this, a.username)}
 
 
 export default Channel;
--- front/features/channel/channelContainer.js
+++ front/features/channel/channelContainer.js
@@ -1,18 +1,13 @@
 import {connect} from 'react-redux'
 import Channel from './Channel'
-import UserList from ''
 
 
 const mapStateToProps = state => {
-	// Object.keys(userObj).map((k)=>console.log(userObj[k]))
 	console.log(state)
-	return{
-		userChannels: state.channel
-}}
-
-const mapStateToProps = (state) => ({
-  usersList: state.usersList 
-})
+	return
+	{
+  	userChatrooms: state.app
+  	}
+}
 
 export default connect(mapStateToProps)(Channel)
-export default connect(mapStateToProps, mapDispatchToProps)(UsersList)
