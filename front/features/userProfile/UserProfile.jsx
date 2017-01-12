36530ea9a1d634c682e580ea567cf693498e32f7
Vanessa Montoya <Vanessa@C4Qs-MacBook-Pro-4.local>
2017-01-12 15:59:49 -0500

    unnecessary code deleted

--- front/features/userProfile/UserProfile.jsx
+++ front/features/userProfile/UserProfile.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import {Link} from 'react-router';
 import store from '../../store/store';
 
-        // <ul>
-        // {props.userChannels.users?
-        //   props.userChannels.users.map((a,key)=>{
-        //   return <li key={key}>{a.username}{a.bio}</li>})
-        //   :false}
-        // </ul>
 const UserProfile = (props)=> {
 	const getList = () => {
     store.dispatch({type:'SHOW_PROFILE', data: false })
